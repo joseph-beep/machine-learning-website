e2bae0ef5b82d6cf9dad22e79ab74daf5ab630d4
Joseph Thomas <110737@st-maartenscollege.nl>
2021-02-09 13:49:04 +0000

    Systeem tussen script en post-pagina geïmplementeerd.

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var title = document.getElementById("post-title");
+var subtitle = document.getElementById("post-subtitle");
+var body = document.getElementById("post-body");
+
+title.innerHTML = "Hello";
+subtitle.innerHTML = "Hello2";
+body.innerHTML = "Hello I am text.";
