87d67182e74a40734965a156d582d88dc92a3fab
Joseph Thomas <110737@st-maartenscollege.nl>
2021-02-09 14:00:08 +0000

    Systeem om .JSON bestanden te lezen is geïmplementeerd.

--- javascript.js
+++ javascript.js
@@ -5,3 +5,8 @@ var body = document.getElementById("post-body");
 title.innerHTML = "Hello";
 subtitle.innerHTML = "Hello2";
 body.innerHTML = "Hello I am text.";
+
+fetch("/posts/test-post.json")
+  .then(response => response.json())
+  .then(data => { title.innerHTML = data.title; body.innerHTML = data.body; subtitle.innerHTML = data.subtitle; })
+  .catch(console.error);
