813e3e6fb3a2b70abef971209be09e1f5797f8a1
Joseph Thomas <110737@st-maartenscollege.nl>
2021-02-09 14:06:03 +0000

    Toegevoegd systeem om URL Parameters lezen om van posts te kiezen.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,31 @@
+function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        parms = {}, i, n, v, nv;
+
+    if (query === url || query === "") return;
+
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=", 2);
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!parms.hasOwnProperty(n)) parms[n] = [];
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
+}
+
+var urlString = window.location.href;
+var urlParams = parseURLParams(urlString);
+
 var title = document.getElementById("post-title");
 var subtitle = document.getElementById("post-subtitle");
 var body = document.getElementById("post-body");
 
-title.innerHTML = "Hello";
-subtitle.innerHTML = "Hello2";
-body.innerHTML = "Hello I am text.";
-
-fetch("/posts/test-post.json")
+fetch("/posts/" + urlParams.post + ".json")
   .then(response => response.json())
   .then(data => { title.innerHTML = data.title; body.innerHTML = data.body; subtitle.innerHTML = data.subtitle; })
   .catch(console.error);
