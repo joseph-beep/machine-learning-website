69762cb1e93926791e45433360804e50fd85f49a
Joseph Thomas <110737@st-maartenscollege.nl>
2021-02-17 14:11:59 +0000

    Verkort de preview post tekst van de home pagina

--- preview-post-loader.js
+++ preview-post-loader.js
@@ -1,10 +1,9 @@
 var texts = document.getElementsByClassName("post-preview");
-
 for (i = 0; i < texts.length; i++) {
   var currentText = texts[i];
 
   fetch("/posts/" + currentText.getAttribute("post") + ".json")
     .then(response => response.json())
-    .then(data => { currentText.innerHTML = data.body; })
+    .then(data => { currentText.innerHTML = data.body.replace(/^(.{200}[^\s]*).*/, "$1") + " ...";})
     .catch(console.error);
 }
