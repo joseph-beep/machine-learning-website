905ce64901025ed47ece4e1db45d1fb41e1ec372
Joseph Thomas <110737@st-maartenscollege.nl>
2021-03-09 17:08:35 +0000

    Fix achtergrond van home-pagina die niet verscheen

--- preview-post-loader.js
+++ preview-post-loader.js
@@ -1,25 +1,14 @@
-var debug = true;
-
 function setValue(url, text) {
   fetch(url)
     .then(response =>
     {
-      if (debug) {
-        console.log(response.url);
-        document.getElementById("printer").innerHTML = response.url;
-      }
       return response.json();
     })
     .then(data =>
     {
       text.innerHTML = data.body.replace(/<\/?("[^"]*"|'[^']*'|[^>])*(>|$)/g, "").replace(/^(.{200}[^\s]*).*/, "$1") + " ...";
     })
-    //.catch (console.error);
-    .catch((error) => {
-      if (debug) {
-        document.getElementById("printer").innerHTML = error;
-      }
-    })
+    .catch (console.error);
 }
 
 var texts = document.getElementsByClassName("post-preview");
