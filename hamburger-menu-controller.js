842c1007ffc1bc4cf6ea6b95a93f26cd31fc9343
Joseph Thomas <110737@st-maartenscollege.nl>
2021-03-15 14:21:08 +0000

    Implementeer hamburger menu om van de zijkant te komen

--- hamburger-menu-controller.js
+++ hamburger-menu-controller.js
@@ -1,10 +1,17 @@
 function toggleHamburger() {
   var hamburgerMenu = document.getElementById("hamburger-menu");
+  var hamburgerButton = document.getElementById("hamburger-button");
 
-  if (hamburgerMenu.style.display == "none") {
-    hamburgerMenu.style.display = "inherit";
+  if (hamburgerMenu.style.width == "40%") {
+    hamburgerMenu.style.width = "0%";
+    hamburgerMenu.style.opacity = "0";
+
+    hamburgerButton.innerHTML = "<p>☰ Menu</p>";
   }
   else {
-    hamburgerMenu.style.display = "none";
+    hamburgerMenu.style.width = "40%";
+    hamburgerMenu.style.opacity = "1";
+
+    hamburgerButton.innerHTML = "<p>🞩 Close</p>";
   }
 }
